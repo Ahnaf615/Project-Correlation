215579320cf5ae88f645f823278cc6b291c32fac
Ahnaf Khan <ahnafkhan123@gmail.com>
2020-11-18 22:44:47 -0500

    changed axises

--- plot.py
+++ plot.py
@@ -7,9 +7,8 @@ Lifetime = data['Life Expectancy']
 PopulationGrowth = data['Population Growth Rate In Percent']
 plt.style.use('seaborn')
 
-plt.scatter(PopulationGrowth, Lifetime, color='grey')
-plt.xlabel('Population growth rate in percent')
-plt.ylabel('Life expectancy in years')
-plt.title('Correlation done by Ahnaf Khan')
+plt.scatter(Lifetime, PopulationGrowth, color='grey')
+plt.xlabel('Life expectancy in years')
+plt.ylabel('Population growth rate in percent')
 plt.savefig('correlation.png')
 plt.show()
